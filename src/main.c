969a5ad01faab17117c4a2bed8ee459ed822e471
LeoTheWizard <leowalk183@gmail.co.uk>
2026-09-01 21:33:05 +0100

    CMake

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @brief Firmware entry point.
+ * 
+ * @author Leo Walker
+ * 
+ * @copyright (c) Leo Walker 2026. All Rights Reserved.
+ */
+
+#include <stdlib.h>
+#include "pico/stdlib.h"
+
+int main()
+{
+    return EXIT_SUCCESS;
+}
